90de4b7f36f56e25dcc0e0e7fbb9576c9c31d21a
Mario <engineer@theboywhoroared.com>
2018-04-21 17:08:33 -0400

    Say Hello directly using the react-dom and JSX

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,16 @@
 // let's go!
+
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
+class StorePicker extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() {
+        return <p>Hello!</p>
+    }
+}
+
+render(<p>Hey!</p>, document.querySelector('#main'))
